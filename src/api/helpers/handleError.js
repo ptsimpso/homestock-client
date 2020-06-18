f64e7a834c3b4982a593433bf8d4eb23b969ea88
Peter Simpson <ptaylorsimpson@gmail.com>
2020-06-18 12:26:05 -0700

    Adds error log

--- src/api/helpers/handleError.js
+++ src/api/helpers/handleError.js
@@ -2,6 +2,7 @@ const handleError = (error) => {
   if (error.response) {
     throw new Error(error.response.data.error);
   } else {
+    console.log(error);
     throw new Error('Something went wrong. Please try again.');
   }
 };
